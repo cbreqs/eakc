05f23dd730be1adef7e89e4a321d263172ada9e4
singularity <singularity@reqs.tech>
2025-11-30 14:36:43 +0000

    Changes to be committed: 	modified:   .idx/dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,16 +1,15 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://firebase.google.com/docs/studio/customize-workspace
+ see: https://firebase.google.com/docs/studio/customize-workspace
 { pkgs, ... }: {
-  # Which nixpkgs channel to use.
+   Which nixpkgs channel to use.
   channel = "stable-24.05"; # or "unstable"
 
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
+     pkgs.go
+     pkgs.python311
+     pkgs.python311Packages.pip
+     pkgs.nodejs_20
+     pkgs.nodePackages.nodemon
   ];
 
   # Sets environment variables in the workspace
@@ -18,14 +17,14 @@
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
-      # "vscodevim.vim"
+       "vscodevim.vim"
     ];
 
     # Enable previews
     previews = {
       enable = true;
       previews = {
-        # web = {
+         web = {
         #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
         #   # and show it in IDX's web preview panel
         #   command = ["npm" "run" "dev"];
